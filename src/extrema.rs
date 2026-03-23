d7bc4ba8226483145e8267ee9a4841edd3d57b06
Timo Markgraf <timo.markgraf@icloud.com>
2026-03-23 19:37:57 +0100

    initial commit

---
+++ src/extrema.rs
@@ -0,0 +1,9 @@
+/*
+use crate::{get_derivative_of_function, root::get_root_of_function};
+
+pub fn calculate_extrema(&function_variables: &Vec<f64>,) -> Result<Vec<f64>, String> {
+    let derivative = Vec::from(get_derivative_of_function(&function_variables));
+
+    get_root_of_function(&derivative, (-1000.0, 1000.0));
+}
+*/
